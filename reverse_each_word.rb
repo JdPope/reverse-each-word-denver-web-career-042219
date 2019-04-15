8ee07e9c5ae5e9b7d70a46e8c36178d82026ed7d
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-15 18:20:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
 def reverse_each_word(sentence)
   new = []
   sentence = sentence.split
-  sentence.each do |word|
+  sentence.collect do |word|
     new<< word.reverse
   end
   return new.join(" ")
 end
+
